f357427e7bbe5951a9b68260698d6bf472e5c945
David Prieto <david.s.prieto1@gmail.com>
2023-01-23 10:51:07 -0600

    solution for leetcode #392 - is subsequence complete

--- leetcode.js
+++ leetcode.js
@@ -1,7 +1,7 @@
 // DAILY LEETCODE CODING EXERCISES:
 
 
-// 01/08/2023 - 1480. Running Sum of 1D Array:
+// 01/08/2023 - #1480. Running Sum of 1D Array:
 //
 // let nums = [1, 2, 3, 4];
 //
@@ -17,7 +17,7 @@
 // console.log(runningSum(nums));
 
 
-// 01/09/2023 - 724. Find Pivot Index:
+// 01/09/2023 - #724. Find Pivot Index:
 //
 // let nums = [-1, -1, 0, 0, -1, -1];
 //
@@ -51,7 +51,7 @@
 // console.log(pivotIndex(nums));
 
 
-// 01/21/2023
+// 01/21/2023 - #205. Isomorphic Strings
 //
 // let s = "paper";
 // let t = "title";
@@ -73,3 +73,25 @@
 // console.log(isIsomorphic(s, t));
 
 
+// 01/23/2023 - #392 - Is Subsequence
+//
+// let s = "abc";
+// let t = "ahbgdc";
+//
+// const isSubsequence = (s, t) => {
+//     let i = 0;
+//     let j = 0;
+//
+//     while (i < s.length) {
+//         if (j === t.length) {
+//             return false;
+//         }
+//         if (s[i] === t[j]) {
+//             i++;
+//         }
+//         j++;
+//     }
+//     return true;
+// }
+//
+// isSubsequence(s, t);
