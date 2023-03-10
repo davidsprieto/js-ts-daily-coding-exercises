198d00b4d7f4ce9b5af6264740e31eb2a93e57c6
David Prieto <david.s.prieto1@gmail.com>
2023-03-10 13:15:16 -0600

    slight refactor to 'string reversal' solution

--- codeup.js
+++ codeup.js
@@ -152,7 +152,7 @@ let s = "I am a computer science student seeking to master my craft of software 
 const reverseStringAgain = (s) => {
   let r = '';
   for (let i = s.length - 1; i >= 0; i--) {
-    r += s[i];
+    r += s.at(i);
   }
   return r;
 }
