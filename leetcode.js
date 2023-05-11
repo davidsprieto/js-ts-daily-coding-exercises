1994bf91418abedf31381f9590554e2e4246d210
David Prieto <david.s.prieto1@gmail.com>
2023-05-11 16:43:09 -0500

    completed leetcode problem #605

--- leetcode.js
+++ leetcode.js
@@ -397,6 +397,7 @@ console.log(gcdOfStrings(str1, str2));
 //  they will have the greatest number of candies among all the kids, or false otherwise.
 //  Note that multiple kids can have the greatest number of candies.
 
+/*
 const kidsWithCandies = (candies, extraCandies) => {
   let array = [];
   let index = 0;
@@ -439,3 +440,31 @@ const kidsWithCandiesRefactored = (candies, extraCandies) => {
 let candies = [2, 3, 5, 1, 3];
 let extraCandies = 3;
 console.log(kidsWithCandiesRefactored(candies, extraCandies));
+ */
+
+
+// 05/11/2023 - #605. Can Place Flowers
+// TODO: You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers cannot be planted in adjacent plots.
+//  Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty, and an integer n, return true if n new
+//  flowers can be planted in the flowerbed without violating the no-adjacent-flowers rule and false otherwise.
+
+
+const canPlaceFlowers = (flowerbed, n) => {
+  if (!n) {
+    return true;
+  }
+  for (let i = 0; i < flowerbed.length; i++) {
+    if (flowerbed[i] === 0 && flowerbed[i - 1] !== 1 && flowerbed[i + 1] !== 1) {
+      n--;
+      flowerbed[i] = 1;
+    }
+    if (!n) {
+      return true;
+    }
+  }
+  return !n;
+}
+
+let flowerbed = [1, 0, 0, 0, 1, 0, 1, 0, 0];
+let n = 1;
+canPlaceFlowers(flowerbed, n);
