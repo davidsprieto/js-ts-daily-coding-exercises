651d19a00853e06677ab6958eef15f5d2048f546
David Prieto <david.s.prieto1@gmail.com>
2023-08-23 18:14:45 -0500

    working on leetcode problem #1657

--- leetcode.js
+++ leetcode.js
@@ -1044,6 +1044,7 @@ console.log(findDifference(nums1, nums2));
 // TODO: Given an array of integers 'arr', return true if the number of occurrences of each value in the array is unique
 //  or false otherwise.
 
+/*
 const uniqueOccurrences = (arr) => {
     const map = new Map([]);
 
@@ -1070,3 +1071,25 @@ const uniqueOccurrences = (arr) => {
 
 let arr = [1, 2, 2, 1, 1, 3];
 console.log(uniqueOccurrences(arr));
+ */
+
+
+// 08/23/2023 - #1657. Determine if Two Strings Are Close
+// TODO: Two strings are considered close if you can attain one from the other using the following operations:
+//  Operation 1: Swap any two existing characters.
+//  For example, abcde -> aecdb
+//  Operation 2: Transform every occurrence of one existing character into another existing character, and do the same with the other character.
+//  For example, aacabb -> bbcbaa (all a's turn into b's, and all b's turn into a's)
+//  You can use the operations on either string as many times as necessary.
+//  Given two strings, word1 and word2, return true if word1 and word2 are close, and false otherwise.
+
+const closeStrings = (word1, word2) => {
+    if (word1.length !== word2.length) {
+        return false;
+    }
+
+}
+
+const getLetterCount = (word) => {
+    const map = {};
+}
